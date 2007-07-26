f7b0a9475e1af33951e3b582379ebe927a22cd45
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-07-26 03:17:23 +0000

    Update migration formats so that they're more maintainable.

--- db/migrate/001_create_countries.rb
+++ db/migrate/001_create_countries.rb
@@ -1,15 +1,15 @@
 class CreateCountries < ActiveRecord::Migration
   def self.up
     create_table :countries do |t|
-      t.column :name,           :string, :null => false, :limit => 80
-      t.column :official_name,  :string, :limit => 80
-      t.column :alpha_2_code,   :string, :null => false, :limit => 2
-      t.column :alpha_3_code,   :string, :null => false, :limit => 3
-      t.column :calling_code,   :string, :limit => 3
+      t.column :name, :string, :null => false, :limit => 80
+      t.column :official_name, :string, :limit => 80
+      t.column :alpha_2_code, :string, :null => false, :limit => 2
+      t.column :alpha_3_code, :string, :null => false, :limit => 3
+      t.column :calling_code, :string, :limit => 3
     end
-    add_index :countries, :name,          :unique => true
-    add_index :countries, :alpha_2_code,  :unique => true
-    add_index :countries, :alpha_3_code,  :unique => true
+    add_index :countries, :name, :unique => true
+    add_index :countries, :alpha_2_code, :unique => true
+    add_index :countries, :alpha_3_code, :unique => true
   end
   
   def self.down
--- db/migrate/002_create_regions.rb
+++ db/migrate/002_create_regions.rb
@@ -1,11 +1,11 @@
 class CreateRegions < ActiveRecord::Migration
   def self.up
     create_table :regions do |t|
-      t.column :country_id,   :integer, :null => false
-      t.column :name,         :string, :null => false, :limit => 50
+      t.column :country_id, :integer, :null => false
+      t.column :name, :string, :null => false, :limit => 50
       t.column :abbreviation, :string, :null => false, :limit => 5
     end
-    add_index :regions, [:name, :country_id],         :unique => true
+    add_index :regions, [:name, :country_id], :unique => true
     add_index :regions, [:abbreviation, :country_id], :unique => true
   end
   
