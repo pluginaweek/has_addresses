5b8dd68ec066b017fc84c9d8834d59ae18e11815
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-07-02 00:57:27 -0400

    Remove rogue assignment of RAILS_FRAMEWORK_ROOT

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,3 @@
-RAILS_FRAMEWORK_ROOT='/home/aaron/Projects/Vendor/rails'
 # Load the plugin testing framework
 $:.unshift("#{File.dirname(__FILE__)}/../../plugin_test_helper/lib")
 require 'rubygems'
