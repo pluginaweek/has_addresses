5e023026caf9d6f595c96f196c31e71e74943796
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-12-14 21:00:06 -0500

    Remove the PluginAWeek namespace

--- lib/has_addresses.rb
+++ lib/has_addresses.rb
@@ -1,18 +1,16 @@
-module PluginAWeek #:nodoc:
-  # Adds a generic implementation for dealing with regions, countries, and
-  # addresses
-  module HasAddresses
-    module MacroMethods
-      # Creates the following association:
-      # * +addresses+ - All addresses associated with the current record.
-      def has_addresses
-        has_many  :addresses,
-                    :as => :addressable
-      end
+# Adds a generic implementation for dealing with regions, countries, and
+# addresses
+module HasAddresses
+  module MacroMethods
+    # Creates the following association:
+    # * +addresses+ - All addresses associated with the current record.
+    def has_addresses
+      has_many  :addresses,
+                  :as => :addressable
     end
   end
 end
 
 ActiveRecord::Base.class_eval do
-  extend PluginAWeek::HasAddresses::MacroMethods
+  extend HasAddresses::MacroMethods
 end
